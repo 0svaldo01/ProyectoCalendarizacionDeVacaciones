b9c9b67591659020da13fc1e648c8ee4b4f510f6
Cecy <171g0218@rcarbonifera.tecnm.mx>
2025-04-15 11:35:28 -0600

    Contexto en el program

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoCalendarizacionDeVacaciones.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
+builder.Services.AddDbContext<ProyectoCfeContext>(options => options.UseMySql("server=localhost;user=root;password=root;database=ProyectoCFE", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql")));
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
 app.Run();
