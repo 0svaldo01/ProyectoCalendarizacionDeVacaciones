9070150938947a0747370e5edba9837c498a6fff
VICTOR OSVALDO RODRIGUEZ HIDALGO <201G0283@rcarbonifera.tecnm.mx>
2025-04-16 00:14:30 -0600

    inicio del proceso para mostrar datos

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoCalendarizacionDeVacaciones.Models;
 
 namespace ProyectoCalendarizacionDeVacaciones.Controllers
 {
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        private readonly ProyectoCfeContext context;
+
+        public UsuarioController(ProyectoCfeContext context)
         {
-            return View();
+            this.context = context;
+        }  
+        public IActionResult Index(int Id)
+        {
+            var data = context.Usuario.OrderBy(x => x.IdUsuario == Id).Include(x => x.IdDepartamentoNavigation).Include(x => x.IdRolNavigation).Include(x => x.IdPuestoNavigation);
+            return View(data);
         }
     }
 }
