19ed073c62e1e7a791fad6198d2568fc1192509d
Cecy <171g0218@rcarbonifera.tecnm.mx>
2025-04-30 15:33:41 -0600

    cambios

---
+++ Areas/Admin/Controllers/PuestosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoCalendarizacionDeVacaciones.Areas.Admin.Controllers
+{
+    public class PuestosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Areas/Admin/Controllers/UsuariosController.cs
+++ Areas/Admin/Controllers/UsuariosController.cs
@@ -16,7 +16,14 @@ namespace ProyectoCalendarizacionDeVacaciones.Areas.Admin.Controllers
         public VacacionescfeContext Context { get; }
 
         #region C.R.U.D
-        
+        #region CREATE
+        [HttpGet]
+        public IActionResult Agregar(Puesto p)
+        {
+            return View();
+        }
+
+        #endregion
         #region READ
         public IActionResult Index()
         {
